d4d6c59588788811c82d61b6cd39b249c4f99792
Jorge Terence <jorge.terence@outlook.com>
2024-08-21 00:44:45 -0300

    move it move it

--- engine.py
+++ engine.py
@@ -1,10 +1,12 @@
 from itertools import chain
 from os import system
+from random import choice
 from time import sleep
 from typing import List, Tuple
 from keyboard import read_event, KEY_DOWN
 
 RED = "\033[91m"
+DUNNO = "\033[92m"
 RESET = "\033[0m"
 
 patterns = {
@@ -60,7 +62,7 @@ class Entity:
         x, y = self.coordinates
         return x <= point[0] <= x + self.width() and y <= point[1] <= y + self.height()
 
-    def colides(self, coords: Tuple[int, int]):
+    def intersects(self, coords: Tuple[int, int]):
         x, y = self.coordinates
         if not self.inbounds(coords):
             return False
@@ -72,15 +74,31 @@ class Entity:
 
         return self.pattern[i][j]
 
+    def colides(self, arena: List[List[int]]):
+        x, y = self.coordinates
+        sample = [row[x : x + self.width()] for row in arena][y : y + self.height()]
+        return any(f and b for f, b in zip(chain(*self.pattern), chain(*sample)))
+
 
 def display(arena: List[List[int]], block: Entity):
     for i in range(len(arena)):
         for j in range(len(arena[0])):
-            if block.colides((j, i)):
+            if arena[i][j]:
+                print("#", end=" ")
+                continue
+            if block.intersects((j, i)):
                 print(f"{block.color}@{RESET}", end=" ")
             else:
                 print(".", end=" ")
         print("")
+
+
+def solidify(arena: List[List[int]], block: Entity):
+    x, y = block.coordinates
+    for i in range(block.height()):
+        for j in range(block.width()):
+
+            arena[y + i][x + j] = arena[y + i][x + j] or block.pattern[i][j]
 
 
 def get_key() -> str:
@@ -94,12 +112,13 @@ if __name__ == "__main__":
     width = 6
     height = 12
     arena = [[0 for _ in range(width)] for _ in range(height)]
+    arena.append([1 for _ in range(width)])
     block = Entity(patterns["T"], RED)
 
-    for tick in range(height):
+    while True:
         display(arena, block)
         print(block.coordinates)
-        sleep(0.5)
+        sleep(0.25)
         key = get_key()
         match key:
             case "left":
@@ -108,12 +127,14 @@ if __name__ == "__main__":
                 block.coordinates[0] += 1
             case "space":
                 block.coordinates[1] = height - 1
-                system("cls")
                 display(arena, block)
-                break
-            case "up":
+            case "r":
                 block.rotate()
-            case "down":
-                block.coordinates[1] += 1
+
+        block.coordinates[1] += 1
+        if block.colides(arena) or block.coordinates[1] == height:
+            block.coordinates[1] -= 1
+            solidify(arena, block)
+            block = Entity(choice(list(patterns.values())), DUNNO, [0, 0])
 
         system("cls")
