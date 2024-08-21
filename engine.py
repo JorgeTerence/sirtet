f9c44588755b465ba42bf8aebb937b16d2f9216b
Jorge Terence <jorge.terence@outlook.com>
2024-08-21 00:10:09 -0300

    added block rendering

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
+from itertools import chain
 from os import system
 from time import sleep
-from typing import List
+from typing import List, Tuple
 from keyboard import read_event, KEY_DOWN
 
 RED = "\033[91m"
@@ -43,14 +44,39 @@ class Entity:
         self.pattern = pattern
         self.color = color
 
+    def width(self):
+        return len(self.pattern[0])
+
+    def height(self):
+        return len(self.pattern)
+
     def rotate(self):
-        pass
+        self.pattern = [
+            [self.pattern[j][i] for j in range(len(self.pattern))]
+            for i in range(len(self.pattern[0]) - 1, -1, -1)
+        ]
+
+    def inbounds(self, point: Tuple[int, int]):
+        x, y = self.coordinates
+        return x <= point[0] <= x + self.width() and y <= point[1] <= y + self.height()
+
+    def colides(self, coords: Tuple[int, int]):
+        x, y = self.coordinates
+        if not self.inbounds(coords):
+            return False
+
+        i, j = [coords[1] - y, coords[0] - x]
+
+        if i >= len(self.pattern) or j >= len(self.pattern[0]):
+            return False
+
+        return self.pattern[i][j]
 
 
 def display(arena: List[List[int]], block: Entity):
     for i in range(len(arena)):
         for j in range(len(arena[0])):
-            if [i, j] == block.coordinates:
+            if block.colides((j, i)):
                 print(f"{block.color}@{RESET}", end=" ")
             else:
                 print(".", end=" ")
@@ -72,22 +98,22 @@ if __name__ == "__main__":
 
     for tick in range(height):
         display(arena, block)
+        print(block.coordinates)
         sleep(0.5)
         key = get_key()
         match key:
             case "left":
-                block.coordinates[1] -= 1
+                block.coordinates[0] -= 1
             case "right":
-                block.coordinates[1] += 1
+                block.coordinates[0] += 1
             case "space":
-                block.coordinates[0] = height - 1
+                block.coordinates[1] = height - 1
                 system("cls")
                 display(arena, block)
                 break
             case "up":
                 block.rotate()
-            case _:
-                print(key)
+            case "down":
+                block.coordinates[1] += 1
 
-        block.coordinates[0] += 1
         system("cls")
